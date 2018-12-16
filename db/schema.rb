d877d80c060d72cc22fc9386aa91186e8205d963
ユウ <sukesuke0718@gmail.com>
2018-12-16 23:39:09 +0900

    new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_135515) do
+ActiveRecord::Schema.define(version: 2018_12_16_142504) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
